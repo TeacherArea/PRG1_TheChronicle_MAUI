a521c2e34006486968639af6ce21844f698e328e
TeacherArea <sewilius.berg@initialize.se>
2024-08-14 14:44:28 +0200

    Add project files.

---
+++ TheChronicle-MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TheChronicle_MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
